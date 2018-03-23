c80789d7ddfd372802a84f044b06b6e078e9c11a
naiieandrade <andradenaiara@hotmail.com>
2018-03-23 18:32:54 -0300

    Fix methods to check max and min

--- main.c
+++ main.c
@@ -104,15 +104,26 @@ Image cut_image(Image img) {
     return cut;
 }
 
-int check_pixel_size_max(int int_pixel) {
-    int pixel_size = 0;
+int check_min(int compare, int number_min) {
+    int number = 0;
 
-    if (255 > int_pixel) {
-      pixel_size = int_pixel;
+    if (compare > number_min) {
+        number = number_min;
     } else {
-      pixel_size = 255;
+        number = compare;
     }
-    return pixel_size;
+    return number;
+}
+
+int check_max(int number_max, int compare) {
+    int number = 0;
+
+    if (number_max > compare) {
+        number = number_max;
+    } else {
+        number = compare;
+    }
+    return number;
 }
 
 Image change_sepia_filter(Image img) {
@@ -127,83 +138,72 @@ Image change_sepia_filter(Image img) {
             pixel[2] = img.pixel[pixel_y][pixel_x][2];
 
             int_pixel =  pixel[0] * .393 + pixel[1] * .769 + pixel[2] * .189;
-            img.pixel[pixel_y][pixel_x][0] = check_pixel_size_max(int_pixel);
+            img.pixel[pixel_y][pixel_x][0] = check_min(255, int_pixel);
 
             int_pixel =  pixel[0] * .349 + pixel[1] * .686 + pixel[2] * .168;
-            img.pixel[pixel_y][pixel_x][1] = check_pixel_size_max(int_pixel);
+            img.pixel[pixel_y][pixel_x][1] = check_min(255, int_pixel);
 
             int_pixel =  pixel[0] * .272 + pixel[1] * .534 + pixel[2] * .131;
-            img.pixel[pixel_y][pixel_x][2] = check_pixel_size_max(int_pixel);
+            img.pixel[pixel_y][pixel_x][2] = check_min(255, int_pixel);
         }
     }
     return img;
 }
 
-int check_size_image(int img, int size, int var) {
-    int size_img;
-
-    if (img - 1 > var + size/2) {
-        size_img = var + size/2;
-    } else {
-        size_img =  img - 1;
-    }
-    return size_img;
-}
-
-int check_positive_number(int size, int var) {
+int get_number_blur() {
     int number = 0;
 
-    if (0 > var-size/2) {
-        number = 0;
-    } else {
-        number = var - size/2;
-    }
+    scanf("%d", &number);
     return number;
 }
 
 int get_color_pixel_per_area(unsigned short int color_pixel, int size) {
     int area = 0;
     area = size * size;
-    return (color_pixel = color_pixel / area);
-}
-
-int get_size_blur() {
-    int size = 0;
-
-    scanf("%d", &size);
-    return size;
+    color_pixel = color_pixel / area;
+    return color_pixel;
 }
 
 Image blur(Image img) {
-    int size = 0;
+    int number_blur = 0;
 
-    size = get_size_blur();
+    number_blur = get_number_blur();
 
     for (unsigned int pixel_y = 0; pixel_y < img.height; ++pixel_y) {
         for (unsigned int pixel_x = 0; pixel_x < img.width; ++pixel_x) {
             Pixel media = {0, 0, 0};
-            int size_height = 0;
-            int size_width = 0;
+            int min_height = 0;
+            int min_width = 0;
             int check_height = 0;
             int check_width = 0;
 
-            size_height = check_size_image(img.height, size, pixel_y);
-            size_width = check_size_image(img.width, size, pixel_x);
+            int min_y = 0;
+            min_y = pixel_y + number_blur/2;
+            int min_x = 0;
+            min_x = pixel_x + number_blur/2;
 
-            check_height = check_positive_number(size,pixel_y);
-            check_width = check_positive_number(size,pixel_x);
+            min_height = check_min(img.height-1, min_y);
+            min_width = check_min(img.width-1, min_x);
 
-            for (int pixel_y = check_height; pixel_y <= size_height; ++pixel_y) {
-                for (int pixel_x = check_width; pixel_x <= size_width; ++pixel_x) {
+            int max_y = 0;
+            max_y = pixel_y - number_blur/2;
+            int max_x = 0;
+            max_x = pixel_x - number_blur/2;
+
+            check_height = check_max(0, max_y);
+            check_width = check_max(0, max_x);
+
+            for (int pixel_y = check_height; pixel_y <= min_height; ++pixel_y) {
+                for (int pixel_x = check_width; pixel_x <= min_width; ++pixel_x) {
                     media.red += img.pixel[pixel_y][pixel_x][0];
                     media.green += img.pixel[pixel_y][pixel_x][1];
                     media.blue += img.pixel[pixel_y][pixel_x][2];
                 }
             }
 
-            img.pixel[pixel_y][pixel_x][0] = get_color_pixel_per_area(media.red, size);
-            img.pixel[pixel_y][pixel_x][1] = get_color_pixel_per_area(media.green, size);
-            img.pixel[pixel_y][pixel_x][2] = get_color_pixel_per_area(media.blue, size);
+            img.pixel[pixel_y][pixel_x][0] = get_color_pixel_per_area(media.red, number_blur);
+            img.pixel[pixel_y][pixel_x][1] = get_color_pixel_per_area(media.green, number_blur);
+            img.pixel[pixel_y][pixel_x][2] = get_color_pixel_per_area(media.blue, number_blur);
         }
     }
     return img;
@@ -226,8 +226,8 @@ Image rotate_90_right(Image img) {
 }
 
 Image rotate_photo(Image img) {
-
     int times = 0;
+
     scanf("%d", &times);
     times = times % 4;
     for (int rotate = 0; rotate < times; ++rotate) {
@@ -236,7 +236,8 @@ Image rotate_photo(Image img) {
     return img;
 }
 
-Image function_mirror(Image img, int horizontal, int inversion_point_x, int inversion_point_y) {
+Image function_mirror(Image img, int horizontal, int inversion_point_x,
+                                                          int inversion_point_y) {
     for (int pixel_y = 0; pixel_y < inversion_point_y; ++pixel_y) {
         for (int pixel_x = 0; pixel_x < inversion_point_x; ++pixel_x) {
             int inversion_point_y = 0;
